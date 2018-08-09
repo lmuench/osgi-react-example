bf68a9122ab500d54aed19ca1968b3c82acab875
Ludwig Muench <muench.ludwig@gmail.com>
2018-08-09 13:02:29 +0200

    use react-data-grid editable example

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -7,6 +7,7 @@ import SensorsContainer from '../containers/SensorsContainer';
 import TopBar from '../components/misc/TopBar';
 import StateView from '../components/misc/StateView';
 import ApiBuilderContainer from './ApiBuilderContainer';
+import ApiBuilderContainer2 from './ApiBuilderContainer2';
 
 const api = config.mockSensorsApi ? sensorsMockApi : sensorsApi;
 
@@ -28,7 +29,7 @@ class App extends Component {
     <div className="App">
       <TopBar path="sensors" items={this.state.sensors} />
       {/* <Route path="/sensors/:name" component={SensorsContainer} /> */}
-      <ApiBuilderContainer />
+      <ApiBuilderContainer2 />
       {config.showStore && <StateView />}
     </div>
   );
